b200f229ab97ee8a6b96af8b8ed158404fba529e
garrisonhh <garrisonhh@pm.me>
2023-01-08 15:13:30 -0500

    add zig version check to build.zig

--- build.zig
+++ build.zig
@@ -1,8 +1,22 @@
 const std = @import("std");
+const builtin = @import("builtin");
 
 pub fn build(b: *std.build.Builder) void {
     const target = b.standardTargetOptions(.{});
     const mode = b.standardReleaseOptions();
+
+    // check compiler version
+    comptime {
+        const desired = try std.SemanticVersion.parse("0.10.0");
+        const version = builtin.zig_version;
+        if (version.order(desired).compare(.neq)) {
+            const msg = std.fmt.comptimePrint(
+                "expected zig version {}, found {}",
+                .{desired, version}
+            );
+            @compileError(msg);
+        }
+    }
 
     const exe = b.addExecutable("fluent", "src/main.zig");
     exe.setTarget(target);
