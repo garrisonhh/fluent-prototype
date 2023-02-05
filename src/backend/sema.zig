c27bc5f61e0c43d64dd22928829909267985811e
garrisonhh <garrisonhh@pm.me>
2023-02-05 11:30:14 -0500

    remove moved functions

--- src/backend/sema.zig
+++ src/backend/sema.zig
@@ -734,43 +734,6 @@ fn analyzeExpr(
     };
 }
 
-/// flattens `do` blocks with one expression into the expression
-fn pruneDoBlocks(env: Env, texpr: *TExpr) void {
-    for (texpr.getChildren()) |*child| {
-        pruneDoBlocks(env, child);
-    }
-
-    if (texpr.data == .call and texpr.data.call[0].isBuiltin(.do)) {
-        const xs = texpr.data.call;
-        if (xs.len == 2) {
-            const child = xs[1];
-            env.ally.free(xs);
-
-            texpr.* = child;
-        }
-    }
-}
-
-/// after analysis, it's possible that types that fluent can't actually lower
-/// are produced (e.g. Any and com type sets like Int). this
-fn verifyDynamic(env: Env, texpr: TExpr) SemaError!void {
-    // check that the class isn't analysis
-    const class = env.tw.get(texpr.ty).classifyRuntime(env.tw);
-
-    if (class == .analysis) {
-        const ty_text = try texpr.ty.writeAlloc(env.ally, env.tw);
-        defer env.ally.free(ty_text);
-
-        const fmt = "inferred type `{s}`, which cannot be executed";
-        return try Message.err(env.ally, TExpr, texpr.loc, fmt, .{ty_text});
-    }
-
-    // recurse
-    for (texpr.getChildren()) |child| {
-        try verifyDynamic(env, child);
-    }
-}
-
 pub fn analyze(
     env: *Env,
     scope: Name,
